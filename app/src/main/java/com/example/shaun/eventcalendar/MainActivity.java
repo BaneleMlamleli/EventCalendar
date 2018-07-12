bc66b86f0aed5b8897fd8b65f8894ff8f9d2c2cd
BaneleMlamleli <mlamlelibanele@yahoo.com>
2018-07-13 01:06:07 +0200

    This is now the master branch

--- app/src/main/java/com/example/shaun/eventcalendar/MainActivity.java
+++ app/src/main/java/com/example/shaun/eventcalendar/MainActivity.java
@@ -8,6 +8,7 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 /**
+ * This is now the develop branch
  * This is now the master branch
  */
 public class MainActivity extends AppCompatActivity {
